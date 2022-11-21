fb5e469f8be20990d8d704443be5cfeaa3705ceb
alvarosang <alvarosangarrido@gmail.com>
2022-11-21 14:12:05 +0100

    Ejercicio_2

---
+++ src/ejerciciosPropuestos/Ejercicio2.java
@@ -0,0 +1,24 @@
+package ejerciciosPropuestos;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		
+		float c = 5f;
+		float d = 5f;
+		
+		if (c < d) {
+			
+			System.out.println(c +" es menor que " + d);
+			
+		} else if (c > d) {
+		
+			System.out.println(c + " es mayor que " + d);
+			
+		} else {
+			
+			System.out.println(" a y b tienen el mismo valor");
+		}
+	}
+
+}
