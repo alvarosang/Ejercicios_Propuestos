ecf3b572af03ee9812dfdecb9f9afffd91d71f6a
alvarosang <alvarosangarrido@gmail.com>
2022-11-21 13:51:46 +0100

    Ejercicios 21/11

---
+++ src/ejerciciosPropuestos/Ejercicio1.java
@@ -0,0 +1,20 @@
+package ejerciciosPropuestos;
+
+public class Ejercicio1 {
+	
+	
+	
+
+	public static void main(String[] args) {
+		
+		int a = 10 , b = 5;
+		
+		System.out.println("El valor de la suma es " + (a + b));
+		System.out.println("El valor de la resta es " + (a - b));
+		System.out.println("El valor de la multiplicación es " + (a * b));
+		System.out.println("El valor de la división es " + ( a / b));
+		System.out.println("El valor del módulo es " + (a % b));
+
+	}
+
+}
